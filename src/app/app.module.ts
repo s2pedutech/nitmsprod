00c16551882027f7e463a713bf07b7a96b247c1d
s2pedutech <gandharpatwardhan@s2pedutech.com>
2018-09-14 19:55:55 +0530

    ui level changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { VerificationComponent } from './verification/verification.component';
+import { HomeComponent } from './home/home.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+const appRoutes: Routes = [
+    {
+        path: '', redirectTo: '/home', pathMatch: 'full'
+    },
+    {
+        path: 'home', component: HomeComponent
+    },
+    {
+        path: 'stuLogin', component: StudentComponent
+    },  
+    {
+        path: 'stuVerify', component: VerificationComponent
+    }
+    ];
+    
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    VerificationComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
