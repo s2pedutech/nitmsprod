4f04312647c98e6ebd1c56fd8283bc5c00363c2c
s2pedutech <gandharpatwardhan@s2pedutech.com>
2018-10-29 16:06:55 +0530

    Added course

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -36,23 +36,54 @@ export class CoursesComponent implements OnInit {
 subcourses: Array<any> = [];
 currentSection = "courseSection";
 courses: Array<any>=[
-{"name":"Management", "img":"assets/img/management.jpg", "subCourse":{"name":"Management Programs", "categories":[
-{
-    
-}
-]}},
+{"name":"Management", "img":"assets/img/management.jpg", "subCourses":[
+    {"name" : "Executive Management Programs","img":"assets/img/ch.jpg" , "syllabus" :"assets/docs/Executive Management Programs.pdf"},
+  {"name" : "Management Programs","img":"assets/img/cs.jpg" ,"syllabus" :"assets/docs/Management Programs.pdf"},
+  {"name" : "Professional Management Programs","img":"assets/img/ch.jpg" , "syllabus" :"assets/docs/Professional Management Programs.pdf"}
+]},
 {"name":"Engineering", "img":"assets/img/engg.jpg", "subCourses" :[
-  {"name" : "Chemical Engineering","img":"assets/img/ch.jpg" , "syllabus" :"assets/docs/chemical.pdf"},
-  {"name" : "Computer Science","img":"assets/img/cs.jpg" ,"syllabus" :"assets/docs/Computer Science.pdf"},
-  {"name" : "Information Technology","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/IT.pdf"}
+  {"name" : "Chemical Engineering","img":"assets/img/ch.jpg" , "syllabus" :"assets/docs/Chemical Engineering.pdf"},
+  {"name" : "Computer Science","img":"assets/img/cs.jpg" ,"syllabus" :"assets/docs/Computer Science Engineering.pdf"},
+  {"name" : "Information Technology","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/IT.pdf"},
+  {
+    "name" : "Automobile Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Automobile Engineering.pdf"
+  },
+  {
+    "name" : "Electrical and Electronics Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Electrical & Electronics Engineering.pdf"
+  },
+  {
+    "name" : "Electrical Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Electrical Engineering.pdf"
+  },
+  {
+    "name" : "Civil Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Civil Engineering.pdf"
+  },
+  {
+    "name" : "Electronics and Telecommunication Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Electronics & Telecommunication Engineering.pdf"
+  },
+  {
+    "name" : "Instrumentation and Control Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Instrumentation & Control Engineering.pdf"
+  },
+  {
+    "name" : "Mechanical Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Mechanical Engineering.pdf"
+  },
+  {
+    "name" : "Polymer Science Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Polymer Science Engineering.pdf"
+  },
 ]},
-{"name":"Diploma/Specialization", "img":"assets/img/diploma.jpg"},
+{"name":"Diploma/Specialization", "img":"assets/img/diploma.jpg", "subCourses" :[
+  {"name" : "One Year Diploma Program","img":"assets/img/ch.jpg" , "syllabus" :"assets/docs/One Year Diploma Program.pdf"},
+  {"name" : "Six Month Diploma Courses","img":"assets/img/ch.jpg" , "syllabus" :"assets/docs/Six Month Diploma Courses.pdf"},
+  {"name" : "Specialization Courses","img":"assets/img/ch.jpg" , "syllabus" :"assets/docs/Specialization List.pdf"}]},
 {"name":"Computer Application", "img":"assets/img/ca.jpg", "subCourses": [
-{ "img":"assets/img/bca.jpg", "name":"","syllabus": "assets/docs/BCA.pdf"},
-{ "name":"","img":"assets/img/mca.png", "syllabus": ""},
+{ "img":"assets/img/bca.jpg", "name":"","syllabus": "assets/docs/Computer Application.pdf"},
+{ "name":"","img":"assets/img/mca.png", "syllabus": "assets/docs/Computer Application.pdf"},
 ]},
-{"name":"Textile Engineering", "img":"assets/img/textileengg.jpg"},
-{"name":"Textile Management", "img":"assets/img/textilemgmt.jpg"},
+{"name":"Textile Engineering", "img":"assets/img/textileengg.jpg", "subCourses": [{
+    "name" : "Textile Engineering","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Textile Engineering.pdf"
+  }]},
+{"name":"Textile Management", "img":"assets/img/textilemgmt.jpg", "subCourses": [{
+    "name" : "Textile Management","img":"assets/img/it.jpg" ,"syllabus" :"assets/docs/Textile Management.pdf"
+  }]},
 ];
   constructor() {
   this.currentSection = "courseSection";
